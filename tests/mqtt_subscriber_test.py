9761d670324c611b3a8d794e193e519b4fe827c4
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-06 16:49:37 +0900

    add tests for mqtt subscriber

---
+++ tests/mqtt_subscriber_test.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from src.mqtt_subscriber import MqttSubscriber
+
+broker_url = 'broker.hivemq.com'
+broker_port = 1883
+
+
+class MqttSubscriberTest(TestCase):
+    def test_creation(self):
+        mqtt_sub = MqttSubscriber(
+            broker_url, broker_port)
+
+        assert mqtt_sub != None
+
+    def test_connection(self):
+        def test_callback(client, userdata, flags, rc):
+            assert rc == 0
+
+        mqtt_sub = MqttSubscriber(
+            broker_url, broker_port, on_connect=test_callback)
+
+    def test_subscription(self):
+        pass
