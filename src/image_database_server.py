6e5411614ce213b8fb69e556ea1b6cec5768d6c2
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-07 16:59:53 +0900

    add database server

---
+++ src/image_database_server.py
@@ -0,0 +1,60 @@
+from src.mqtt_subscriber import MqttSubscriber, ConnectCallback, _on_connect_default as _on_connect_mqtt
+from src.mongo_handler import MongoHandler, _on_connect_default as _on_connect_db
+
+import jsonschema
+import json
+from os import path
+
+import sys
+
+
+class ImageDataBaseServer():
+    '''
+        throws jsonschema.ValidationError for incorrect config
+    '''
+
+    def __init__(self, config: json):
+        self.config = self._validate_input(config)
+
+    def run(self, on_connect_db: ConnectCallback = _on_connect_db, on_connect_mqtt: ConnectCallback = _on_connect_mqtt):
+        self.mongo_handler = self._create_mongo_handler(
+            self.config['mongo_handler'], on_connect_db)
+        self.mqtt_sub = self._create_mqtt_sub(
+            self.config['mqtt_subscriber'], on_connect_mqtt)
+
+    def _validate_input(self, config):
+        current_dir = path.dirname(path.abspath(__file__))
+        schema_path = path.join(current_dir, 'config_schema.json')
+        with open(schema_path) as schema_file:
+            schema = json.load(schema_file)
+        jsonschema.validate(config, schema)
+        return config
+
+    def _create_mongo_handler(self, config, on_connect):
+        return MongoHandler(config['connection_string'], config['database'], on_connect)
+
+    def _create_mqtt_sub(self, config, on_connect):
+        def on_message(msg):
+            # todo: provide filename with mqtt object sent
+            self.mongo_handler.storeImage(msg, 'any.jpg')
+        mqtt_sub = MqttSubscriber(
+            config['broker_url'], config['broker_port'], on_connect=on_connect)
+        mqtt_sub.subscribe(config['topic'], on_message)
+        return mqtt_sub
+
+
+def main(config_file):
+    with open(config_file) as file:
+        config = json.loads(file.read())
+    image_db_server = ImageDataBaseServer(config)
+    image_db_server.run()
+    while True:
+        try:
+            pass
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == '__main__':
+    config_file = sys.argv[1]
+    main(config_file)
--- src/mongo_handler.py
+++ src/mongo_handler.py
@@ -3,6 +3,9 @@ from pymongo.errors import PyMongoError
 
 from gridfs import GridFS
 
+from os import path
+import sys
+
 from typing import NewType, Callable
 
 
@@ -21,14 +24,14 @@ class MongoHandler():
         todo: docu write interface and methods
     """
 
-    def __init__(self, connection_string, database, on_connect=_on_connect_default):
+    def __init__(self, connection_string: str, database: str, on_connect: ConnectCallback = _on_connect_default):
         self.client = MongoClient(connection_string)
         self._check_connection(on_connect)
         self.database = self.client[database]
 
-    def storeImage(self, collection, image, file_name) -> bool:
+    def storeImage(self, collection: str, image_data: str, file_name: str) -> bool:
         fs = GridFS(self.database, collection=collection)
-        file_id = fs.put(image, filename=file_name)
+        file_id = fs.put(image_data, filename=file_name)
         if fs.exists(file_id):
             return True
         return False
@@ -39,3 +42,19 @@ class MongoHandler():
             on_connect(True, '')
         except PyMongoError as e:
             on_connect(False, str(e))
+
+
+def main(connection_string, database, img_path, img_name):
+    mongo_handler = MongoHandler(connection_string, database)
+    complete_path = path.join(img_path, img_name)
+    with open(complete_path, 'rb') as file:
+        success = mongo_handler.storeImage(database, file, img_name)
+    print("success" if success else "failed")
+
+
+if __name__ == '__main__':
+    connection_string = sys.argv[1]
+    database = sys.argv[2]
+    img_path = sys.argv[3]
+    img_name = sys.argv[4]
+    main(connection_string, database, img_path, img_name)
