ff354ff8f012675f2dd3ebba964a5a6ae2039405
Justin Gruenenwald <Justin.Gruenenwald@faro.com>
2023-05-07 12:04:11 +0900

    add error msg to mqtt subscriber on_connect

--- src/mqtt_subscriber.py
+++ src/mqtt_subscriber.py
@@ -11,18 +11,21 @@ class MutableFlag:
         self.value = value
 
 
-def _on_connect_default(rc):
-    if rc == 0:
-        print('connected successfully')
+def _on_connect_default(connected, error_msg):
+    if connected:
+        print('mqtt subsciber connected successfully')
     else:
-        print(f'failed to connect with code {rc}')
+        print(f'failed to connect mqtt subsciber because: {error_msg}')
 
 
-ConnectCallback = NewType('ConnectCallback', Callable[[int], None])
+ConnectCallback = NewType('ConnectCallback', Callable[[bool, str], None])
 MessageCallback = NewType('OnMessageCallback', Callable[[bytes], None])
 
 
 class MqttSubscriber:
+    """
+    """
+
     def __init__(self, broker_url: str, broker_port: int, id: str = str(uuid4()), on_connect: ConnectCallback = _on_connect_default):
         self._client = self._connect_mqtt(
             broker_url, broker_port, id, on_connect)
@@ -52,9 +55,15 @@ class MqttSubscriber:
         self._wait_on_callback(client, connect_flag)
 
     def _wrap_on_connect(self, on_connect, connect_flag):
+        rc_msgs = {1: 'incorrect protocol version',
+                   2: 'invalid client identifier',
+                   3: 'server unavailable',
+                   4: 'bad username or password',
+                   5: 'not authorized'}
+
         def flagged_on_connect(client, userdata, flags, rc):
             connect_flag.value = True
-            on_connect(rc)
+            on_connect(rc == 0, rc_msgs.get(rc, 'unknown'))
         return flagged_on_connect
 
     def _wait_on_callback(self, client, connect_flag):
--- tests/mqtt_subscriber_test.py
+++ tests/mqtt_subscriber_test.py
@@ -4,35 +4,35 @@ from src.mqtt_subscriber import MqttSubscriber
 from .test_publisher import MqttTestPublisher
 
 
-broker_url = 'broker.hivemq.com'
-broker_port = 1883
-topic = 'one/unique/nice/test/topic'
+BROKER_URL = 'broker.hivemq.com'
+BROKER_PORT = 1883
+TOPIC = 'one/unique/nice/test/topic'
 
 
 class MqttSubscriberTest(TestCase):
     def test_creation(self):
         mqtt_sub = MqttSubscriber(
-            broker_url, broker_port)
+            BROKER_URL, BROKER_PORT)
 
         assert mqtt_sub != None
 
     def test_connection(self):
-        def test_callback(rc):
-            assert rc == 0
+        def test_callback(connected, _):
+            assert connected
 
-        mqtt_sub = MqttSubscriber(
-            broker_url, broker_port, on_connect=test_callback)
+        _ = MqttSubscriber(
+            BROKER_URL, BROKER_PORT, on_connect=test_callback)
 
     def test_subscription(self):
         sent_message = 'test_message'
-        publisher = MqttTestPublisher(broker_url, broker_port)
+        publisher = MqttTestPublisher(BROKER_URL, BROKER_PORT)
 
         def test_on_message(msg):
             assert msg == sent_message
 
         mqtt_sub = MqttSubscriber(
-            broker_url, broker_port)
+            BROKER_URL, BROKER_PORT)
 
-        mqtt_sub.subscribe(topic, test_on_message)
+        mqtt_sub.subscribe(TOPIC, test_on_message)
 
-        publisher.publish(topic, sent_message)
+        publisher.publish(TOPIC, sent_message)
